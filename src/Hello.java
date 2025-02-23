01a449111770d098ee4fb4751a4f6976f3088a31
Alejandro2718 <alejandrojimenez2718@gmail.com>
2025-02-23 17:02:03 +0100

    commit #4

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     {
         System.out.println("Hello World");
         System.out.println("Hello World3");
+        System.out.println("Hello");
+        System.out.println("This is Alejandro");
     }
 }
